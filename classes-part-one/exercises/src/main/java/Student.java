c96b1cc568b6a27fcc51d7621fb1b6250b897ca7
Vincent Kipkoech <Mail.Vincentk@gmail.com>
2024-09-12 02:28:40 -0500

    Chapter 4 Exercise

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -7,4 +7,46 @@ public class Student {
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
+
+        public Student(String name, int studentId, int numberOfCredits, double gpa) {
+                this.name = name;
+                this.studentId = studentId;
+                this.numberOfCredits = numberOfCredits;
+                this.gpa = gpa;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public void setStudentId(int studentId) {
+                this.studentId = studentId;
+        }
+
+        public void setNumberOfCredits(int numberOfCredits) {
+                this.numberOfCredits = numberOfCredits;
+        }
+
+        public void setGpa(double gpa) {
+                this.gpa = gpa;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public int getStudentId() {
+                return studentId;
+        }
+
+        public int getNumberOfCredits() {
+                return numberOfCredits;
+        }
+
+        public double getGpa() {
+                return gpa;
+        }
+
+
+
 }
